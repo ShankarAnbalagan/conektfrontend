67cfe2a84bad1ac890b99c4451d46c2cc9bf3187
Radhikaagal <radhika.agal@gmail.com>
2019-11-03 17:49:00 +0530

    first

---
+++ src/components/dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class dashboard extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+
+        </div> );
+    }
+}
+ 
+export default dashboard;
---
+++ src/components/profile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class profile extends Component {
+    state = {  }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default profile;
